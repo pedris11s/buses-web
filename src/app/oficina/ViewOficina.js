68bbacbc9bb6f7d826958ec3db55c96339675863
Pedro Torres Paredes <ptorres@estudiantes.uci.cu>
2018-10-23 07:35:10 -0400

    viewOficina adding coop to oficina

--- src/app/oficina/ViewOficina.js
+++ src/app/oficina/ViewOficina.js
@@ -2,6 +2,10 @@ import React from 'react';
 import axios from "axios";
 import {API_ROOT} from "../../config";
 
+import { Button, Table,Row, Col, Card, CardHeader, CardBody } from 'reactstrap';
+import AddCoopOficina from './AddCoopOficina';
+
+
 export default class ViewOficina extends React.Component{
 
   constructor(props){
@@ -23,8 +27,48 @@ export default class ViewOficina extends React.Component{
 
   render(){
     return (
-      <div>
-        YOY SOY LA VISTA!
+      <div className="animated fadeIn">
+        <Row>
+          <Col>
+            <Card>
+              <CardHeader>
+                <strong><i className="icon-info pr-1"></i>Oficina id: {this.state.oficina.id}</strong>
+              </CardHeader>
+              <CardBody>
+                <Table responsive striped hover>
+                  <tbody>
+                  <tr>
+                    <td>Nombre:</td>
+                    <td><strong>{this.state.oficina.nombre}</strong></td>
+                  </tr>
+                  <tr>
+                    <td>Ciudad:</td>
+                    <td><strong>{this.state.oficina.ciudad}</strong></td>
+                  </tr>
+                  <tr>
+                    <td>Direccion:</td>
+                    <td><strong>{this.state.oficina.direccion}</strong></td>
+                  </tr>
+                  <tr>
+                    <td>Telefono:</td>
+                    <td><strong>{this.state.oficina.telefono}</strong></td>
+                  </tr>
+                  <tr>
+                    <td>Facebook:</td>
+                    <td><strong>{this.state.oficina.facebook}</strong></td>
+                  </tr>
+                  <tr>
+                    <td>Whatsapp:</td>
+                    <td><strong>{this.state.oficina.whatsapp}</strong></td>
+                  </tr>
+                  </tbody>
+                </Table>
+                <AddCoopOficina oficina={this.state.oficina}/>
+
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
       </div>
     )
   }
