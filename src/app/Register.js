ef046070907fd9af27baf6f00f6757d5a2959d46
Pedro Torres Paredes <ptorres@estudiantes.uci.cu>
2018-12-07 05:50:34 -0500

    fixing register view

--- src/app/Register.js
+++ src/app/Register.js
@@ -121,16 +121,6 @@ class Register extends Component {
                     <Button type="submit" color="success" block>Create Account</Button>
                   </Form>
                 </CardBody>
-                <CardFooter className="p-4">
-                  <Row>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-facebook" block><span>facebook</span></Button>
-                    </Col>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-twitter" block><span>twitter</span></Button>
-                    </Col>
-                  </Row>
-                </CardFooter>
               </Card>
             </Col>
           </Row>
